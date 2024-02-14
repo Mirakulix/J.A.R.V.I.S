b2ff13d20932c87d3edbab4f906e2dddd9368e19
fscholz <florian.scholz@wu.ac.at>
2024-02-14 08:37:45 +0100

    Add new files and configurations

---
+++ ai-models/start_huggingface.sh
@@ -0,0 +1,14 @@
+pip install --upgrade pip
+pip install transformers datasets
+pip install torch
+pip install tensorflow
+
+# Install the TensorFlow pip package dependencies (if using a virtual environment, omit the --user argument):
+
+
+pip install -U --user pip numpy wheel packaging requests opt_einsum
+pip install -U --user keras_preprocessing --no-deps
+
+
+docker pull ollama/ollama
+
